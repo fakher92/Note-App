67c0dc72dfcdfd040f98854e74c4cf50ea61ee8f
Fakher Alhamad <fakher1281992@gmail.com>
2018-06-07 14:59:17 +0200

    Restarting App with Nodemon

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 console.log('Starting app.');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-// console.log(_.isString(true));
-// console.log(_.isString('Fakher'));
-var filteredArray = _.uniq(['Andrew', 1, 'Fakher', 1, 2, 3, 4]);
-console.log(filteredArray);
-
-//console.log('Result:', notes.add(9, -2));
-
-// var user = os.userInfo();
-
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
