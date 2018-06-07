2291f0ded1ff596e42a89d4f5c535730254945c6
Fakher Alhamad <fakher1281992@gmail.com>
2018-06-07 14:17:28 +0200

    Using 3rd Party Modules

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log('Result:', notes.add(9, -2));
+// console.log(_.isString(true));
+// console.log(_.isString('Fakher'));
+var filteredArray = _.uniq(['Andrew', 1, 'Fakher', 1, 2, 3, 4]);
+console.log(filteredArray);
+
+//console.log('Result:', notes.add(9, -2));
 
 // var user = os.userInfo();
 
