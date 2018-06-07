87a7553932adcf059ba5527d0358ef1ce60e2f91
Fakher Alhamad <fakher1281992@gmail.com>
2018-06-07 15:26:57 +0200

    Simplified Input With Yargs

--- notes.js
+++ notes.js
@@ -1,5 +1,21 @@
 console.log('Starting notes.js');
 
+var addNote = (title, body) => {
+  console.log('Adding note', title, body);
+};
+
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var getNote = title => {
+  console.log('Getting note', title);
+};
+
+var removeNote = title => {
+  console.log('Removing note', title);
+};
+
 module.exports.addNote = () => {
   console.log('addNote');
   return 'New note';
